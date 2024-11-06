efb0859498aaee710e20c6a418c6281dbb2fbcf8
rhuang367 <rhuang367@gatech.edu>
2024-11-06 13:49:08 -0500

    update experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -28,7 +28,11 @@ function Experience() {
             >
                 <h3 className='vertical-timeline-element-title'>SoftworksAI, Queens, New York</h3>
                 <h4 className='vertical-timeline-element-subtitle'>System QA Internship</h4>
-                <p>maintain QA and develop internal QA tool for auto QA steps for data verification</p>
+                <p>Implemented automation solutions using Bash and Python for testing, reducing manual effort, accelerating test cycles, and improving software quality.</p>
+                <p>Classified and verified data using Python (pandas) to enhance data models.</p>
+                <p>Managed dataset curation for address verification, developed requirements, test strategies, and oversaw bug reporting using Jira.</p>
+                <p>Analyzed test data to uncover insights, visualize trends, and generate reports for stakeholders, aiding decision-making and QA monitoring.</p>
+                <p>Fostered team collaboration to ensure efficient project workflows and successful outcomes.</p>
             </VerticalTimelineElement>
             <VerticalTimelineElement className='vertical-timeline-element--work' 
             date='2022.03 - 2023.07'
@@ -39,12 +43,14 @@ function Experience() {
             >
                 <h3 className='vertical-timeline-element-title'>True AI, Queens, New York</h3>
                 <h4 className='vertical-timeline-element-subtitle'>Software Engineer</h4>
-                <p>Debugged a C++ software solution on Windows for mortgage data extraction using OCR technology, 
-                    designed a versatile QA tool using PyQt5, Shell Script, and Python for automation, 
-                    actively contributed to SDLC processes, enhanced Data Model accuracy, 
-                    pioneered comprehensive data processes, monitored data pipelines, 
-                    collaborated on innovative business solutions, 
-                    and executed patching plans for software robustness.</p>
+                <p>Resolved C++ solution errors on Windows for OCR-based mortgage data extraction, significantly enhancing system reliability and performance.</p>
+                <p>Designed and implemented solutions for mortgage file data extraction, improving accuracy through optimized data processing techniques.</p>
+                <p>Developed and optimized C++ functions to refine input parameter handling, boosting system efficiency.</p>
+                <p>Built a comprehensive QA automation tool using QT, Bash, XML/JSON, and Python, increasing processing speed by 50%.</p>
+                <p>Managed QA processes, including internal tools for data transfer, file management, and test method execution, improving workflow efficiency.</p>
+                <p>Leveraged Python and XML/JSON for data analysis, validation, and visualization, improving data ingestion, transformation, and loading for large-scale repositories.</p>
+                <p>Monitored data pipelines, proactively addressing performance bottlenecks and enhancing data model accuracy.</p>
+                <p>Contributed to the design, testing, and maintenance of the software development lifecycle (SDLC), improving extraction accuracy and system performance.</p>
             </VerticalTimelineElement>
             <VerticalTimelineElement className='vertical-timeline-element--education' 
             date='2023 - Present'
@@ -55,7 +61,7 @@ function Experience() {
             >
                 <h3 className='vertical-timeline-element-title'>Georgia Institute of Technology, Atlanta, GA</h3>
                 <h4 className='vertical-timeline-element-subtitle'>Master of Science</h4>
-                <p>Computer Science(AI/ML)</p>
+                <p>Master of Science in Computer Science of Interactive Intelligence(AI)</p>
             </VerticalTimelineElement>
         </VerticalTimeline>
     </div>
