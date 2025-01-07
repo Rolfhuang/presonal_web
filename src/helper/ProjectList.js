d4fb4d1b87c6f6c4e59d9c3a766611cb40cfb7ec
rhuang367 <rhuang367@gatech.edu>
2025-01-07 17:14:51 -0500

    DNA project

--- src/helper/ProjectList.js
+++ src/helper/ProjectList.js
@@ -1,10 +1,16 @@
 import Dguy from "../assets/delivery_guy.jpg";
+import DNAseq from "../assets/DL.jpg";
 
 export const projectList = [
     {
         name: 'Game',
         image: Dguy,
         skills: 'C#, Unity, Json',
+    },
+    {
+        name: 'Deep Learning Model Project',
+        image: DNAseq,
+        skills: 'Neural Network Architecture, CNNs, Transformer, LSTM, Python, PyTorch, Pandas, Numpy',
     },
 
 ];
